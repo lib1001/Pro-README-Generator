e211c65be8bf750aa75dbcf268dd7150a52f261b
Lauren <lburke522@gmail.com>
2022-09-07 20:54:25 -0400

    added license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,28 +20,54 @@ function renderLicenseBadge(license) {
   }else {
     return licenseBadge = ''
   }
-
-  // [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]
-
-  // [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
-
-  // [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]
-
-  // [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]
-
 }
 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+  let licenseChoice = license;
+  let licenseLink = '';
+  
+  if (licenseChoice === 'Apache License 2.0') {
+    licenseLink = '(https://opensource.org/licenses/Apache-2.0)'
+    return licenseLink
+  } else if (licenseChoice === 'MIT License') {
+    licenseLink = '(https://opensource.org/licenses/MIT)'
+    return licenseLink
+  } else if (licenseChoice === 'Boost Software License 1.0') {
+    licenseLink = '(https://opensource.org/licenses/BSL-1.0)'
+    return licenseLink
+  } else if (licenseChoice === 'GNU Public License v3.0') {
+    licenseLink = '(https://opensource.org/licenses/GPL-3.0)'
+    return licenseLink
+  }else {
+    return licenseLink = ''
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
+// function renderLicenseSection(license) {
+// if (license === license) {
+//   return '${data.license}'
+// } else {
+//   return ''
+// }
 
+  // let licenseInfo = license;
+  
+  // if (licenseInfo === 'Apache License 2.0') {
+  //   return 'Apache License 2.0'
+  // } else if (licenseInfo === 'MIT License') {
+  //   return 'MIT License'
+  // } else if (licenseInfo === 'Boost Software License 1.0') {
+  //   return 'Boost Software License 1.0'
+  // } else if (licenseInfo === 'GNU Public License v3.0') {
+  //   return 'GNU Public License v3.0'
+  // }else {
+  //   return ''
+  // }
 }
 
 // TODO: Create a function to generate markdown for README
@@ -70,7 +96,9 @@ ${data.installation}
 ${data.usage}
 
 ## License:
-${data.license}
+${renderLicenseSection(data.license)}\n
+${renderLicenseLink(data.license)}
+
 
 ## Credits:
 ${data.credits}
